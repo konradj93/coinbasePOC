d3bfc476c5239652d522de10edc1907d378aab31
Konrad Jarek <konradj@hexocean.com>
2024-02-20 20:49:26 +0100

    poc

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,117 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CBPayInstanceType, InitOnRampParams, initOnRamp } from '@coinbase/cbpay-js';
+import { Dialog, DialogActions, DialogContent, DialogTitle, Grid } from '@mui/material';
+import Button from '@mui/material/Button';
+import React, { useRef, useState } from 'react';
 
-function App() {
+const WALLET_ADDRESS = ""
+const API_KEY = ""
+const App = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <CoinbaseButton openModal={handleClickOpen} />
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Coinbase Modal</DialogTitle>
+        <DialogContent>
+          <Grid container height="100%" overflow="scroll">
+            <Grid item xs={12} className="onramp-modal__right-side">
+              <div
+                className="onramp-modal__sardine-wrapper"
+                id="coinbase"
+              ></div>
+            </Grid>
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Close
+          </Button>
+          {/* Additional actions can be added here */}
+        </DialogActions>
+      </Dialog>
     </div>
   );
-}
+};
+
+export const CoinbaseButton = ({ openModal }: { openModal: () => void }) => {
+  const onrampInstance = useRef<CBPayInstanceType | null>();
+  const onRampInstanceOptions: InitOnRampParams = {
+    appId: API_KEY,
+    target: "#cbpay-container",
+    widgetParameters: {
+      destinationWallets: [
+        {
+          address: WALLET_ADDRESS,
+          blockchains: ["base"],
+        },
+      ],
+    },
+    embeddedContentStyles: {
+      target: "#coinbase",
+      height: "500px",
+      width: "400px",
+      position: "static",
+    },
+    onSuccess: () => {
+      // handle navigation when user successfully completes the flow
+    },
+    onExit: () => {
+      // handle navigation from dismiss / exit events due to errors
+    },
+    onEvent: (event) => {
+      // event stream
+      // eslint-disable-next-line no-console
+      console.log(event, "Event");
+    },
+    experienceLoggedIn: "popup",
+    experienceLoggedOut: "embedded",
+  };
+
+  const handleOnPress = () => {
+    if (onrampInstance.current) {
+      onrampInstance.current.destroy();
+    }
+
+    initOnRamp(onRampInstanceOptions, (error, instance) => {
+      if (instance) {
+        onrampInstance.current = instance;
+        openModal();
+        instance.open();
+      }
+      // eslint-disable-next-line no-console
+      console.log(error);
+    });
+  };
+  const destroy = () => {
+    if (onrampInstance.current) {
+      onrampInstance.current.destroy();
+      onrampInstance.current = null;
+    }
+  };
+  return (
+    <div>
+      <Button onClick={handleOnPress}>Initiate Coinbase Action</Button>
+      <Button onClick={destroy}>Destroy</Button>
+    </div>
+  );
+};
+
+
 
 export default App;
